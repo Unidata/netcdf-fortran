8170549749b14f78e89de5cfb5f4ce4c1f8fe6c3
Ed Hartnett <ed@unidata.ucar.edu>
2011-10-03 14:39:34 +0000

    updated version numbers to 4.2

--- nf_test/create_fills.sh
+++ nf_test/create_fills.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# This shell script which creates the fill.nc file from fill.cdl.
-# $Id: create_fills.sh,v 1.2 2009/01/25 14:33:45 ed Exp $
+# This shell script which copies the fill.nc file from the source dist.
 
 echo
-echo "*** Testing creating file with fill values."
+echo "*** Copying file with fill values."
 set -e
-ncgen -b $srcdir/fills.cdl
+cp $srcdir/ref_fills.nc fills.nc
 echo "*** SUCCESS!"
 exit 0
