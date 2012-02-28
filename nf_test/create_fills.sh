ef2013441ca53b514a8c62ee5bc6e5f60a4c6bde
Dennis Heimbigner <dmh@unidata.ucar.edu>
2012-02-28 22:08:51 +0000

    switch to 2003: part 1

--- nf_test/create_fills.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This shell script which copies the fill.nc file from the source dist.
-
-echo
-echo "*** Copying file with fill values."
-set -e
-cp $srcdir/ref_fills.nc fills.nc
-echo "*** SUCCESS!"
-exit 0
