54633b511f9b96cb5ee8052268a0c78fdbbd3c54
Russ Rew <russ@unidata.ucar.edu>
2013-10-08 09:39:12 -0600

    Made added error codes consistent with C netcdf.h

--- fortran/netcdf_constants.f90
+++ fortran/netcdf_constants.f90
@@ -80,86 +80,90 @@
   ! error codes:
   !
   integer, parameter, public :: &
-    nf90_noerr        = 0,      &
-    nf90_ebadid       = -33,    &
-    nf90_eexist       = -35,    &
-    nf90_einval       = -36,    &
-    nf90_eperm        = -37,    &
-    nf90_enotindefine = -38,    &
-    nf90_eindefine    = -39,    &
-    nf90_einvalcoords = -40,    &
-    nf90_emaxdims     = -41,    &
-    nf90_enameinuse   = -42,    &
-    nf90_enotatt      = -43,    &
-    nf90_emaxatts     = -44,    &
-    nf90_ebadtype     = -45,    &
-    nf90_ebaddim      = -46,    &
-    nf90_eunlimpos    = -47,    &
-    nf90_emaxvars     = -48,    &
-    nf90_enotvar      = -49,    &
-    nf90_eglobal      = -50,    &
-    nf90_enotnc       = -51,    &
-    nf90_ests         = -52,    &
-    nf90_emaxname     = -53,    &
-    nf90_eunlimit     = -54,    &
-    nf90_enorecvars   = -55,    &
-    nf90_echar        = -56,    &
-    nf90_eedge        = -57,    &
-    nf90_estride      = -58,    &
-    nf90_ebadname     = -59,    &
-    nf90_erange       = -60,    &
-    nf90_enomem       = -61,    &
-    nf90_evarsize     = -62,    &
-    nf90_edimsize     = -63,    &
-    nf90_etrunc       = -64
-
+    nf90_noerr        = 0,      & ! No Error
+    nf90_ebadid       = -33,    & ! Not a valid ID
+    nf90_eexist       = -35,    & ! netcdf file exists && NF90_NOCLOBBER
+    nf90_einval       = -36,    & ! Invalid Argument
+    nf90_eperm        = -37,    & ! Write to read only
+    nf90_enotindefine = -38,    & ! Operation not allowed in data mode
+    nf90_eindefine    = -39,    & ! Operation not allowed in define mode
+    nf90_einvalcoords = -40,    & ! Index exceeds dimension bound
+    nf90_emaxdims     = -41,    & ! NC_MAX_DIMS exceeded
+    nf90_enameinuse   = -42,    & ! String match to name in use
+    nf90_enotatt      = -43,    & ! Attribute not found
+    nf90_emaxatts     = -44,    & ! NC_MAX_ATTRS exceeded
+    nf90_ebadtype     = -45,    & ! Not a netcdf data type
+    nf90_ebaddim      = -46,    & ! Invalid dimension id or name
+    nf90_eunlimpos    = -47,    & ! NC_UNLIMITED in the wrong index
+    nf90_emaxvars     = -48,    & ! NC_MAX_VARS exceeded
+    nf90_enotvar      = -49,    & ! The variable ID is invalid for the specified netCDF dataset.
+    nf90_eglobal      = -50,    & ! Action prohibited on NC_GLOBAL varid
+    nf90_enotnc       = -51,    & ! Not a netcdf file
+    nf90_ests         = -52,    & ! In Fortran, string too short
+    nf90_emaxname     = -53,    & ! NC_MAX_NAME exceeded
+    nf90_eunlimit     = -54,    & ! NC_UNLIMITED size already in use
+    nf90_enorecvars   = -55,    & ! nc_rec op when there are no record vars
+    nf90_echar        = -56,    & ! Attempt to convert between text & numbers
+    nf90_eedge        = -57,    & ! Start+count exceeds dimension bound
+    nf90_estride      = -58,    & ! Illegal stride
+    nf90_ebadname     = -59,    & ! Attribute or variable name contains illegal characters
+    nf90_erange       = -60,    & ! Math result not representable
+    nf90_enomem       = -61,    & ! Memory allocation (malloc) failure
+    nf90_evarsize     = -62,    & ! One or more variable sizes violate format constraints 
+    nf90_edimsize     = -63,    & ! Invalid dimension size
+    nf90_etrunc       = -64,    & ! File likely truncated or possibly corrupted
+    nf90_eaxistype    = -65       ! Unknown axis type.
   !
-  ! more error codes for DAP and netCDF-4:
+  ! more error codes for DAP
   !
   integer, parameter, public :: &
-    nf90_edap         = -66,    &
-    nf90_ecurl        = -67,    &
-    nf90_eio          = -68,    &
-    nf90_enodata      = -69,    &
-    nf90_edapsvc      = -70,    &
-    nf90_edas         = -71,    &
-    nf90_edds         = -72,    &
-    nf90_edatadds     = -73,    &
-    nf90_edapurl      = -74,    &
-    nf90_edapconstraint = -75,  &
-    nf90_etranslation = -76,    &
-    nf904_first_error = -100,    &
-    nf90_ehdferr      = -101,    &
-    nf90_ecantread    = -102,    &
-    nf90_ecantwrite   = -103,    &
-    nf90_ecantcreate  = -104,    &
-    nf90_efilemeta    = -105,    &
-    nf90_edimmeta     = -106,    &
-    nf90_eattmeta     = -107,    &
-    nf90_evarmeta     = -108,    &
-    nf90_enocompound  = -109,    &
-    nf90_eattexists   = -110,    &
-    nf90_enotnc4      = -111,    &
-    nf90_estrictnc3   = -112,    &
-    nf90_enotnc3      = -113,    &
-    nf90_enopar       = -114,    &
-    nf90_eparinit     = -115,    &
-    nf90_ebadgrpid    = -116,    &
-    nf90_ebadtypid    = -117,    &
-    nf90_etypdefined  = -118,    &
-    nf90_ebadfield    = -119,    &
-    nf90_ebadclass    = -120,    &
-    nf90_emaptype     = -121,    &
-    nf90_elatefill    = -122,    &
-    nf90_elatedef     = -123,    &
-    nf90_edimscale    = -124,    &
-    nf90_enogrp       = -125,    &
-    nf90_estorage     = -126,    &
-    nf90_ebadchunk    = -127,    &
-    nf90_enotbuilt    = -128,    &
-    nf90_ediskless    = -129,    &
-    nf90_ecantextend  = -130,    &
-    nf90_empi         = -131,    &
+    nf90_edap         = -66,    & ! Generic DAP error
+    nf90_ecurl        = -67,    & ! Generic libcurl error
+    nf90_eio          = -68,    & ! Generic IO error
+    nf90_enodata      = -69,    & ! Attempt to access variable with no data
+    nf90_edapsvc      = -70,    & ! DAP server error
+    nf90_edas         = -71,    & ! Malformed or inaccessible DAS
+    nf90_edds         = -72,    & ! Malformed or inaccessible DDS
+    nf90_edatadds     = -73,    & ! Malformed or inaccessible DATADDS
+    nf90_edapurl      = -74,    & ! Malformed DAP URL
+    nf90_edapconstraint = -75,  & ! Malformed DAP Constrain
+    nf90_etranslation = -76,    & ! Untranslatable construct
+    nf904_first_error = -100
+  !
+  ! error codes for netCDF-4
+  !
+integer, parameter, public :: &
+    nf90_ehdferr      = -101,    & ! Error at HDF5 layer. 
+    nf90_ecantread    = -102,    & ! Can't read. 
+    nf90_ecantwrite   = -103,    & ! Can't write. 
+    nf90_ecantcreate  = -104,    & ! Can't create. 
+    nf90_efilemeta    = -105,    & ! Problem with file metadata. 
+    nf90_edimmeta     = -106,    & ! Problem with dimension metadata. 
+    nf90_eattmeta     = -107,    & ! Problem with attribute metadata. 
+    nf90_evarmeta     = -108,    & ! Problem with variable metadata. 
+    nf90_enocompound  = -109,    & ! Not a compound type. 
+    nf90_eattexists   = -110,    & ! Attribute already exists. 
+    nf90_enotnc4      = -111,    & ! Attempting netcdf-4 operation on netcdf-3 file.   
+    nf90_estrictnc3   = -112,    & ! Attempting netcdf-4 operation on strict nc3 netcdf-4 file.   
+    nf90_enotnc3      = -113,    & ! Attempting netcdf-3 operation on netcdf-4 file.   
+    nf90_enopar       = -114,    & ! Parallel operation on file opened for non-parallel access.   
+    nf90_eparinit     = -115,    & ! Error initializing for parallel access.   
+    nf90_ebadgrpid    = -116,    & ! Bad group ID.   
+    nf90_ebadtypid    = -117,    & ! Bad type ID.   
+    nf90_etypdefined  = -118,    & ! Type has already been defined and may not be edited. 
+    nf90_ebadfield    = -119,    & ! Bad field ID.   
+    nf90_ebadclass    = -120,    & ! Bad class.   
+    nf90_emaptype     = -121,    & ! Mapped access for atomic types only.   
+    nf90_elatefill    = -122,    & ! Attempt to define fill value when data already exists. 
+    nf90_elatedef     = -123,    & ! Attempt to define var properties, like deflate, after enddef. 
+    nf90_edimscale    = -124,    & ! Probem with HDF5 dimscales.
+    nf90_enogrp       = -125,    & ! No group found.
+    nf90_estorage     = -126,    & ! Can't specify both contiguous and chunking.
+    nf90_ebadchunk    = -127,    & ! Bad chunksize.
+    nf90_enotbuilt    = -128,    & ! Attempt to use feature that was not turned on when netCDF was built.
+    nf90_ediskless    = -129,    & ! Error in using diskless  access. 
+    nf90_ecantextend  = -130,    & ! Attempt to extend dataset during ind. I/O operation. 
+    nf90_empi         = -131,    & ! MPI operation failed. 
     nf904_last_error  = -131
 
   !
@@ -232,32 +236,6 @@ integer, parameter, public :: &
      nf90_shuffle = 1, &
      nf90_szip_ec_option_mask = 4, &
      nf90_szip_nn_option_mask = 32
-
-! Extra error codes.
-integer, parameter, public :: &
-     nf90_ehdferr = -101, & ! Error at HDF5 layer. 
-     nf90_ecantread = -102, & ! Can't read. 
-     nf90_ecantwrite = -103, & ! Can't write. 
-     nf90_ecantcreate = -104, & ! Can't create. 
-     nf90_efilemeta = -105, & ! Problem with file metadata. 
-     nf90_edimmeta = -106, & ! Problem with dimension metadata. 
-     nf90_eattmeta = -107, & ! Problem with attribute metadata. 
-     nf90_evarmeta = -108, & ! Problem with variable metadata. 
-     nf90_enocompound = -109, & ! Not a compound type. 
-     nf90_eattexists = -110, & ! Attribute already exists. 
-     nf90_enotnc4 = -111, & ! Attempting netcdf-4 operation on netcdf-3 file.   
-     nf90_estrictnc3 = -112, & ! Attempting netcdf-4 operation on strict nc3 netcdf-4 file.   
-     nf90_enotnc3 = -113, & ! Attempting netcdf-3 operation on netcdf-4 file.   
-     nf90_enopar = -114, & ! Parallel operation on file opened for non-parallel access.   
-     nf90_eparinit = -115, & ! Error initializing for parallel access.   
-     nf90_ebadgrpid = -116, & ! Bad group ID.   
-     nf90_ebadtypid = -117, & ! Bad type ID.   
-     nf90_etypdefined = -118, & ! Type has already been defined and may not be edited. 
-     nf90_ebadfield = -119, & ! Bad field ID.   
-     nf90_ebadclass = -120, & ! Bad class.   
-     nf90_emaptype = -121, & ! Mapped access for atomic types only.   
-     nf90_elatefill = -122, & ! Attempt to define fill value when data already exists. 
-     nf90_elatedef = -122 ! Attempt to define var properties, like deflate, after enddef. 
 
 ! This is the position of NC_NETCDF4 in cmode, counting from the
 ! right, starting (uncharacteristically for fortran) at 0. It's needed
