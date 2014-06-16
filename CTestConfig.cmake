e993344aa9115a7e47a9cba99f1978ce2c34a32a
Ward Fisher <wfisher@unidata.ucar.edu>
2014-06-16 15:02:34 -0600

    Cleaned out old utility scripts.  Changed name of cmake templates to fit standard convention.  Added default CTestConfig file, required by Continuous Integration.  Using a generated one doesn't work in that case.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,10 +8,10 @@
 
 set(DART_TESTING_TIMEOUT "4800")
 
-set(CTEST_PROJECT_NAME "netcdf-c")
+set(CTEST_PROJECT_NAME "netcdf-fortran")
 set(CTEST_NIGHTLY_START_TIME "02:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org") 
-set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-c")
+set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-fortran")
 set(CTEST_DROP_SITE_CDASH TRUE)
