badce7dde349e5f9e84328550e6b9316f39d3fb1
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-03-20 12:15:48 -0600

    got f90tst_parallel_fill working

--- nf_test/f90tst_parallel_fill.f90
+++ nf_test/f90tst_parallel_fill.f90
@@ -25,14 +25,14 @@ program f90tst_parallel_fill
   integer :: var_type(NUM_VARS) = (/ nf90_byte, nf90_short, nf90_int, &
        nf90_float, nf90_double, nf90_ubyte, nf90_ushort, nf90_uint /)
   integer :: x_dimid, y_dimid
-  integer :: byte_out(HALF_NY, HALF_NX), byte_in(NY, NX)
-  integer :: short_out(HALF_NY, HALF_NX), short_in(NY, NX)
-  integer :: int_out(HALF_NY, HALF_NX), int_in(NY, NX)
-  real :: areal_out(HALF_NY, HALF_NX), areal_in(NY, NX)
-  real :: double_out(HALF_NY, HALF_NX), double_in(NY, NX)
-  integer :: ubyte_out(HALF_NY, HALF_NX), ubyte_in(NY, NX)
-  integer :: ushort_out(HALF_NY, HALF_NX), ushort_in(NY, NX)
-  integer (kind = EightByteInt) :: uint_out(HALF_NY, HALF_NX), uint_in(NY, NX)
+  integer :: byte_out(HALF_NY, HALF_NX), byte_in(HALF_NY, HALF_NX)
+  integer :: short_out(HALF_NY, HALF_NX), short_in(HALF_NY, HALF_NX)
+  integer :: int_out(HALF_NY, HALF_NX), int_in(HALF_NY, HALF_NX)
+  real :: areal_out(HALF_NY, HALF_NX), areal_in(HALF_NY, HALF_NX)
+  real :: double_out(HALF_NY, HALF_NX), double_in(HALF_NY, HALF_NX)
+  integer :: ubyte_out(HALF_NY, HALF_NX), ubyte_in(HALF_NY, HALF_NX)
+  integer :: ushort_out(HALF_NY, HALF_NX), ushort_in(HALF_NY, HALF_NX)
+  integer (kind = EightByteInt) :: uint_out(HALF_NY, HALF_NX), uint_in(HALF_NY, HALF_NX)
   integer :: nvars, ngatts, ndims, unlimdimid, file_format
   integer :: x, y, v
   integer :: start_out(MAX_DIMS), count_out(MAX_DIMS)
@@ -102,7 +102,7 @@ program f90tst_parallel_fill
   else if (my_rank .eq. 3) then
      start_out = (/ HALF_NX + 1, HALF_NY + 1 /)
   endif
-  print *, my_rank, start_out, count_out
+  ! print *, my_rank, start_out, count_out
 
   ! Write this processor's data, except for processor zero.
   call check(nf90_put_var(ncid, varid(1), byte_out, start = start_out, count = count_out))
@@ -121,12 +121,12 @@ program f90tst_parallel_fill
   call check(nf90_open(FILE_NAME, nf90_nowrite, ncid, comm = MPI_COMM_WORLD, &
        info = MPI_INFO_NULL))
   
-! Check some stuff out.
+  ! Check some stuff out.
   call check(nf90_inquire(ncid, ndims, nvars, ngatts, unlimdimid, file_format))
   if (ndims /= 2 .or. nvars /= NUM_VARS .or. ngatts /= 0 .or. unlimdimid /= -1 .or. &
        file_format /= nf90_format_netcdf4) stop 2
 
-! Now each processor will read one quarter of the whole array.
+  ! Now each processor will read one quarter of the whole array.
   count_in = (/ HALF_NX, HALF_NY /)
   if (my_rank .eq. 0) then
      start_in = (/ 1, 1 /)
@@ -138,7 +138,7 @@ program f90tst_parallel_fill
      start_in = (/ HALF_NX + 1, HALF_NY + 1 /)
   endif
 
-! Read this processor's data.
+  ! Read this processor's data.
   call check(nf90_get_var(ncid, varid(1), byte_in, start = start_in, count = count_in))
   call check(nf90_get_var(ncid, varid(2), short_in, start = start_in, count = count_in))
   call check(nf90_get_var(ncid, varid(3), int_in, start = start_in, count = count_in))
@@ -148,31 +148,31 @@ program f90tst_parallel_fill
   call check(nf90_get_var(ncid, varid(7), ushort_in, start = start_in, count = count_in))
   call check(nf90_get_var(ncid, varid(8), uint_in, start = start_in, count = count_in))
 
-!   ! Check the data. All the data from the processor zero are fill
-!   ! value.
-!   do x = 1, NX
-!      do y = 1, NY
-!         if (my_rank .eq. 0) then
-!            if (byte_in(y, x) .ne. -1) stop 13
-!            if (short_in(y, x) .ne. -2) stop 14
-!            if (int_in(y, x) .ne. -4) stop 15
-!            if (areal_in(y, x) .ne. 2.5) stop 16
-!            if (double_in(y, x) .ne. -4.5) stop 17
-!            if (ubyte_in(y, x) .ne. 1) stop 18
-!            if (ushort_in(y, x) .ne. 2) stop 19
-!            if (uint_in(y, x) .ne. 4) stop 20
-!         else 
-!            if (byte_in(y, x) .ne. nf90_fill_byte) stop 3
-!            if (short_in(y, x) .ne. nf90_fill_short) stop 4
-!            if (int_in(y, x) .ne. nf90_fill_int) stop 5
-!            if (areal_in(y, x) .ne. nf90_fill_real) stop 6
-!            if (double_in(y, x) .ne. nf90_fill_double) stop 7
-!            if (ubyte_in(y, x) .ne. nf90_fill_ubyte) stop 8
-!            if (ushort_in(y, x) .ne. nf90_fill_ushort) stop 9
-!            if (uint_in(y, x) .ne. nf90_fill_uint) stop 10
-!         endif
-!      end do
-!   end do
+  ! Check the data. All the data from the processor zero are fill
+  ! value.
+  do x = 1, HALF_NX
+     do y = 1, HALF_NY
+        if (my_rank .ne. 0) then
+           if (byte_in(y, x) .ne. -1) stop 13
+           if (short_in(y, x) .ne. -2) stop 14
+           if (int_in(y, x) .ne. -4) stop 15
+           if (areal_in(y, x) .ne. 2.5) stop 16
+           if (double_in(y, x) .ne. -4.5) stop 17
+           if (ubyte_in(y, x) .ne. 1) stop 18
+           if (ushort_in(y, x) .ne. 2) stop 19
+           if (uint_in(y, x) .ne. 4) stop 20
+        else
+           if (byte_in(y, x) .ne. nf90_fill_byte) stop 3
+           if (short_in(y, x) .ne. nf90_fill_short) stop 4
+           if (int_in(y, x) .ne. nf90_fill_int) stop 5
+           if (areal_in(y, x) .ne. nf90_fill_real) stop 6
+           if (double_in(y, x) .ne. nf90_fill_double) stop 7
+           if (ubyte_in(y, x) .ne. nf90_fill_ubyte) stop 8
+           if (ushort_in(y, x) .ne. nf90_fill_ushort) stop 9
+           if (uint_in(y, x) .ne. nf90_fill_uint) stop 10
+        endif
+     end do
+  end do
 
 ! Close the file.
   call check(nf90_close(ncid))
