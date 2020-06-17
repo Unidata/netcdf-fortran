0dce92efa5609398b584cf0ab6510b80ecfed115
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-06-17 12:36:09 -0600

    further test development

--- nf03_test4/f90tst_parallel_compressed.F90
+++ nf03_test4/f90tst_parallel_compressed.F90
@@ -16,12 +16,12 @@ program f90tst_parallel_compressed
 
   ! File info.
   character (len = *), parameter :: FILE_NAME = "f90tst_parallel_compressed.nc"
-  integer :: ncid
+  integer :: ncid, oldMode
 
   ! Dimensions
   integer, parameter :: NUM_DIMS = 5
   character (len = *), parameter :: dim_name(NUM_DIMS) = &
-       (/ 'grid_xt', 'grid_xy', 'pfull  ', 'phalf  ', 'time   ' /)
+       (/ 'grid_xt', 'grid_yt', 'pfull  ', 'phalf  ', 'time   ' /)
   integer, dimension(NUM_DIMS) :: dim_len = (/ 3072, 1536, 127, 128, 1 /)
   integer :: dimid(NUM_DIMS)
 
@@ -33,13 +33,19 @@ program f90tst_parallel_compressed
   integer :: var_type(NUM_VARS) = (/ nf90_double, nf90_double, nf90_double, &
        nf90_double, nf90_float, nf90_float, nf90_double, nf90_float /)
   integer :: var_ndims(NUM_VARS) = (/ 1, 2, 1, 2, 1, 1, 1, 4 /)
-
+  real, allocatable :: value_phalf(:)
+  real, allocatable :: value_pfull(:)
+  real, allocatable :: value_grid_xt(:)
+  real, allocatable :: value_grid_yt(:)
+  real, allocatable :: value_lon(:,:)
+  real, allocatable :: value_lat(:,:)
+  
   ! These are for checking file contents.
   character (len = 128) :: name_in
   integer :: dim_len_in, xtype_in, ndims_in
   integer :: nvars, ngatts, ndims, unlimdimid, file_format
   integer :: x, y, v
-  integer :: p, my_rank, i, ierr
+  integer :: p, my_rank, i, j, ierr
 
   call MPI_Init(ierr)
   call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr)
@@ -50,46 +56,109 @@ program f90tst_parallel_compressed
   endif
 
   ! There must be 4 procs for this test.
-  if (p .ne. 4) then
-     print *, 'Sorry, this test program must be run on four processors.'
-     stop 1
-  endif
+  ! if (p .ne. 4) then
+  !    print *, 'Sorry, this test program must be run on four processors.'
+  !    stop 1
+  ! endif
 
+  allocate(value_pfull(dim_len(3)))
+  allocate(value_phalf(dim_len(4)))
+  allocate(value_grid_xt(dim_len(1)))
+  allocate(value_grid_yt(dim_len(2)))
+  allocate(value_lat(dim_len(2), dim_len(1)))
+  allocate(value_lon(dim_len(2), dim_len(1)))
+  
+  ! Some fake data to write.
+  do i = 1, dim_len(3)
+     value_pfull(i) = i
+  end do
+  do i = 1, dim_len(4)
+     value_phalf(i) = i
+  end do
+  do i = 1, dim_len(1)
+     value_grid_xt(i) = i
+  end do
+  do i = 1, dim_len(2)
+     value_grid_yt(i) = i
+  end do
+  do i = 1, dim_len(1)
+     do j = 1, dim_len(2)
+        value_lat(i, j) = i + j
+        value_lon(i, j) = i + j
+     end do
+  end do
+    
   ! Create the netCDF file.
   call check(nf90_create(FILE_NAME, nf90_netcdf4, ncid, comm = MPI_COMM_WORLD, &
        info = MPI_INFO_NULL))
+  call check(nf90_set_fill(ncid, NF90_NOFILL, oldMode))
 
-  ! Define the dimensions.
-  do i = 1, NUM_DIMS
-     ierr = add_dim(ncid, trim(dim_name(i)), dimid(i), dim_len(i))
-     if (ierr .ne. 0) stop 10
-  end do
+  ! Define dimension grid_xt.
+  call check(nf90_def_dim(ncid, trim(dim_name(1)), dim_len(1), dimid(1)))
 
-  ! Define the variables.
+  ! Define variable grid_xt and write data.
   call check(nf90_def_var(ncid, trim(var_name(1)), var_type(1), dimids=(/dimid(1)/), varid=varid(1)))
   call check(nf90_var_par_access(ncid, varid(1), NF90_INDEPENDENT))
+  call check(nf90_enddef(ncid))
+  call check(nf90_put_var(ncid, varid(1), values=value_grid_xt))
+  call check(nf90_redef(ncid))
 
+  ! Define dimension grid_yt.
+  call check(nf90_def_dim(ncid, trim(dim_name(2)), dim_len(2), dimid(2)))
+
+  ! Define variable lat and write data(?)
   call check(nf90_def_var(ncid, trim(var_name(2)), var_type(2), dimids=(/dimid(1),  dimid(2)/), varid=varid(2)))
-  call check(nf90_var_par_access(ncid, varid(2), NF90_INDEPENDENT))
+  ! call check(nf90_var_par_access(ncid, varid(2), NF90_INDEPENDENT))
+  ! call check(nf90_enddef(ncid))
+  ! call check(nf90_put_var(ncid, varid(2), values=value_lat))
+  ! call check(nf90_redef(ncid))
 
+  ! Define variable grid_yt and write data.
   call check(nf90_def_var(ncid, trim(var_name(3)), var_type(3), dimids=(/dimid(2)/), varid=varid(3)))
   call check(nf90_var_par_access(ncid, varid(3), NF90_INDEPENDENT))
+  call check(nf90_enddef(ncid))
+  call check(nf90_put_var(ncid, varid(3), values=value_grid_yt))
+  call check(nf90_redef(ncid))
 
+  ! Define variable lon and write data (?)
   call check(nf90_def_var(ncid, trim(var_name(4)), var_type(4), dimids=(/dimid(1), dimid(2)/), varid=varid(4)))
-  call check(nf90_var_par_access(ncid, varid(4), NF90_INDEPENDENT))
+  ! call check(nf90_var_par_access(ncid, varid(4), NF90_INDEPENDENT))
+  ! call check(nf90_enddef(ncid))
+  ! call check(nf90_put_var(ncid, varid(1), values=value_lon))
+  ! call check(nf90_redef(ncid))
 
+  ! Define dimension pfull.
+  call check(nf90_def_dim(ncid, trim(dim_name(3)), dim_len(3), dimid(3)))
+
+  ! Define variable pfull and write data.
   call check(nf90_def_var(ncid, trim(var_name(5)), var_type(5), dimids=(/dimid(3)/), varid=varid(5)))
   call check(nf90_var_par_access(ncid, varid(5), NF90_INDEPENDENT))
+  call check(nf90_enddef(ncid))
+  call check(nf90_put_var(ncid, varid(5), values=value_pfull))
+  call check(nf90_redef(ncid))
 
+  ! Define dimension phalf.
+  call check(nf90_def_dim(ncid, trim(dim_name(4)), dim_len(4), dimid(4)))
+  
+  ! Define variable phalf and write data.
   call check(nf90_def_var(ncid, trim(var_name(6)), var_type(6), dimids=(/dimid(4)/), varid=varid(6)))
   call check(nf90_var_par_access(ncid, varid(6), NF90_INDEPENDENT))
+  call check(nf90_enddef(ncid))
+  call check(nf90_put_var(ncid, varid(6), values=value_phalf))
+  call check(nf90_redef(ncid))
 
+  ! Define dimension time.
+  call check(nf90_def_dim(ncid, trim(dim_name(5)), dim_len(5), dimid(5)))
+
+  ! Define variable time and write data.
   call check(nf90_def_var(ncid, trim(var_name(7)), var_type(7), dimids=(/dimid(5)/), varid=varid(7)))
   call check(nf90_var_par_access(ncid, varid(7), NF90_INDEPENDENT))
+  call check(nf90_enddef(ncid))
+  call check(nf90_redef(ncid))
 
+  ! Define variable clwmr and write data (?)
   call check(nf90_def_var(ncid, trim(var_name(8)), var_type(8), dimids=(/dimid(1), dimid(2), dimid(3), dimid(5)/), varid=varid(8)))
   call check(nf90_var_par_access(ncid, varid(8), NF90_INDEPENDENT))
-  
 
   ! Close the file.
   call check(nf90_close(ncid))
@@ -117,13 +186,20 @@ program f90tst_parallel_compressed
      if (ndims_in .ne. var_ndims(i)) stop 112
   end do
 
-
   ! Close the file.
   call check(nf90_close(ncid))
 
-  call MPI_Finalize(ierr)
+  ! Free resources.
+  deallocate(value_pfull)
+  deallocate(value_phalf)
+  deallocate(value_grid_xt)
+  deallocate(value_grid_yt)
+  deallocate(value_lat)
+  deallocate(value_lon)
 
   if (my_rank .eq. 0) print *,'*** SUCCESS!'
+
+  call MPI_Finalize(ierr)
 
 contains
   !     This subroutine handles errors by printing an error message and
