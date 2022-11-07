95106362f33f5d0085ab8a4daced2b9831356be9
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2022-11-07 11:30:56 -0700

    Update fortran/netcdf_constants.F90
    Co-authored-by: Matthew Thompson <fortran@gmail.com>

--- fortran/netcdf_constants.F90
+++ fortran/netcdf_constants.F90
@@ -256,7 +256,7 @@ integer, parameter, public :: &
      nf90_noquantize = 0, &
      nf90_quantize_bitgroom = 1, &
      nf90_quantize_granularbr = 2, &
-     nf90_quantize_bitround = 1
+     nf90_quantize_bitround = 3
 
 ! This is the position of NC_NETCDF4 in cmode, counting from the
 ! right, starting (uncharacteristically for fortran) at 0. It's needed
